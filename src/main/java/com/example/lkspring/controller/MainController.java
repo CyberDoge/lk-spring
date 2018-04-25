12989c977afc6d5fa765ceacff25fba764b02d71
CyberDoge <tagirman@gmail.com>
2018-04-25 23:14:21 +0400

    adding edit

--- src/main/java/com/example/lkspring/controller/MainController.java
+++ src/main/java/com/example/lkspring/controller/MainController.java
@@ -1,19 +1,29 @@
 package com.example.lkspring.controller;
 
-import com.example.lkspring.model.User;
 import com.example.lkspring.sevice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @GetMapping("/")
     public ModelAndView welcome() {
@@ -23,23 +33,38 @@ public class MainController {
     @RequestMapping(value = {"/user/", "/user/home", "/user/profile"}, method = RequestMethod.GET)
     public ModelAndView userPage() {
         ModelAndView model = new ModelAndView();
-        var userDet = (org.springframework.security.core.userdetails.User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var user = userService.findByUsername(userDet.getUsername());
+        var user = userService.findByUsername(getUser().getUsername());
         model.addObject("user", user);
         model.setViewName("/user/profile");
         return model;
     }
 
     @RequestMapping(value = "/user/edit", method = RequestMethod.GET)
-    public ModelAndView editPage(){
-        var user = userService.findByUsername(((org.springframework.security.core.userdetails.User)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
-        var mov =  new ModelAndView("/user/edit", "name", user.getUsername());
-        mov.addObject("email", user.getEmail());
+    public ModelAndView editPage(ModelAndView mov, BindingResult errors) {
+        mov.addObject("name", getUser().getUsername());
+        mov.setViewName("/user/edit");
+        mov.addObject("errors", errors);
         return mov;
     }
+
     @RequestMapping(value = "/user/edit", method = RequestMethod.POST)
-    public ModelAndView endEditing(){
-        return null;
+    public String endEditing(@RequestBody Model model, HttpServletRequest request, RedirectAttributes redir, BindingResult errors) {
+        var user = userService.findByUsernameAndPassword(getUser().getUsername(), bCryptPasswordEncoder.encode(request.getParameter("old_password")));
+        if (user == null) errors.rejectValue("badpassword", "wrong password");
+        if (userService.findByUsername(request.getParameter("name")) != null)
+            errors.rejectValue("badusername", "this username is already taken");
+        if (request.getParameter("new password").length() != 0 && request.getParameter("new_password").length() > 6 && request.getParameter("new_password").length() < 32)
+            errors.rejectValue("password", "password have to be longer then 6 and shorter then 32");
+        if (request.getParameter("new password").length() != 0 && !request.getParameter("confirm_password").equals(request.getParameter("new_password")))
+            errors.rejectValue("confirm", "the password are not confirm");
+        if (errors.hasErrors()) {
+            redir.addFlashAttribute("errors", errors);
+            return "redirect:/user/edit";
+        }
+        user.setUsername(request.getParameter("name"));
+        user.setPassword(bCryptPasswordEncoder.encode(request.getParameter("new_password")));
+        userService.update(user);
+        return "redirect:/user/home";
     }
 
     @RequestMapping(value = "/user/game", method = RequestMethod.GET)
@@ -49,17 +74,22 @@ public class MainController {
     }
 
     @PostMapping(value = "/user/endGame")
-    public void endGame(@RequestBody User user) {
+    public void endGame(@RequestBody com.example.lkspring.model.User user) {
         var postUser = userService.findByUsername(user.getUsername());
-        if(postUser.getMaxScore() < user.getMaxScore()){
+        if (postUser.getMaxScore() < user.getMaxScore()) {
             postUser.setMaxScore(user.getMaxScore());
             userService.update(postUser);
         }
         return;
     }
+
     @RequestMapping(value = "top", method = RequestMethod.GET)
-    public ModelAndView printTop(){
-        var list =  userService.top10MaxScore();
+    public ModelAndView printTop() {
+        var list = userService.top10MaxScore();
         return new ModelAndView("top", "list", list);
+    }
+
+    private User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
--- src/main/webapp/WEB-INF/jsp/user/edit.jsp
+++ src/main/webapp/WEB-INF/jsp/user/edit.jsp
@@ -1,16 +1,30 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Edit profile</title>
 </head>
 <body>
-<form:form method="post" modelAttribute="user" action="${save}">
-    username: <input type="text" placeholder="${name}" name="username" id="username" >
+<c:if test="${not empty errors}">
+    <ul>
+        <c:forEach var="errorElement" items="${errors.getAllErrors()}">
+            <li>${errorElement.getCode()}</li>
+        </c:forEach>
+    </ul>
+</c:if>
+<form:form method="post">
+    <label for="username">username:</label>
+    <input type="text" id="username" name="username" placeholder="username" value="${name}"/>
     <br/>
-    old password: <input type="password" name="password" id="password" >
+    <label for="old_password">old password:</label>
+    <input type="password" id="old_password" name="old_password" placeholder="old password"/>
     <br/>
-    new password: <input type="password" name="confirmPassword" id="confirmPassword" >
+    <label for="new_password">new password:</label>
+    <input type="password" id="new_password" name="new_password" placeholder="new password"/>
+    <br/>
+    <label for="confirm_password">new password:</label>
+    <input type="password" id="confirm_password" name="confirm_password" placeholder="confirm password"/>
     <br/>
     <button type="submit">Edit</button></form:form>
 <form:form/>
